ebbaedfc918cd13318fd6637caa7adbb47b2daf3
Vishnu <iamwishnu@gmail.com>
2016-01-28 20:15:11 +0530

    Complete main Activity Layout

--- app/src/main/java/com/vn/portfolio/MainActivity.java
+++ app/src/main/java/com/vn/portfolio/MainActivity.java
@@ -2,6 +2,8 @@ package com.vn.portfolio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonSpotify = (Button) findViewById(R.id.button1);
+        Button buttonScores = (Button) findViewById(R.id.button2);
+        Button buttonLibrary = (Button) findViewById(R.id.button3);
+        Button buttonBuildBig = (Button) findViewById(R.id.button4);
+        Button buttonXyz = (Button) findViewById(R.id.button5);
+        Button buttonCapstone = (Button) findViewById(R.id.button6);
+
+    }
+
+    public void display(View view) {
     }
 }
